e9828ae45c5cbf3f26b50b6642efa2973a225c5d
Christi Gilreath <130399626+christigilreath@users.noreply.github.com>
2025-03-27 15:02:32 -0400

    created homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,20 @@
+import styles from "./HomePage.module.css";
+import imageURL from "../../assets/homepageImg.jpg";
 function HomePage() {
-  return <p>Home</p>;
+  return (
+    <section>
+      <img src={imageURL} alt="homepage image" />
+      <div className={styles.description}>
+        <h2>Welcome to My Fake Store!</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
+          a, tempora tempore porro, sunt iure deleniti ullam eligendi veniam
+          mollitia assumenda id voluptas! Minus quibusdam rerum molestias soluta
+          illo eius.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default HomePage;
